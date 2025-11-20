a4f30c8aff901bbc72182f9af01eb50b3faa4493
utkarsh797691 <utkarshpatel5355@gmail.com>
2025-11-20 16:18:04 +0530

    16:17

---
+++ lib/repository/screens/bottomnav/bottomnavscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class BottomNavScreen extends StatefulWidget {
+  const BottomNavScreen({super.key});
+
+  @override
+  State<BottomNavScreen> createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  int currentIndex=0;
+  List<Widget>pages=[
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/repository/screens/print/printscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PrintScreen extends StatelessWidget {
+  const PrintScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(child: Text("Print Screen"),),
+    );
+  }
+}
